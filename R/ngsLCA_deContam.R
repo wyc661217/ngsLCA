85b16e766ec74202a2fa96c65d7c0f812ebb81e6
Yucheng Wang <40031671+wyc661217@users.noreply.github.com>
2023-09-05 17:35:55 +0100

    Update ngsLCA_deContam.R

--- R/ngsLCA_deContam.R
+++ R/ngsLCA_deContam.R
@@ -11,7 +11,6 @@
 #' @param threshold.3_control for generating contamination list, minimum sum of reads across all blank controls required for keeping a taxon; default is 5.
 #'
 #' @return An updated complete_profile.txt (after removing contaminations).
-#' @importFrom tidyr separate %>%
 #' @importFrom utils read.csv read.delim write.table
 #' @export
 #'
